0889acb6e306e741aff9ef3dcf36a6117ceb95a3
juanonsoftware <juanonsoftware@github.com>
2016-03-23 17:50:14 +0700

    ImgurTesting

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Rabbit.Net.Imgur;
+using System;
+using System.IO;
+
+namespace ImgurTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uploader = new ImgurUploader();
+            var result = uploader.Upload("fb39e7076e0e8cd",
+                  new ImageData(File.ReadAllBytes(@"C:\Users\Public\Pictures\Sample Pictures\Desert.jpg"))
+                  {
+                      Title = "Testing",
+                      Description = "A Testing DESC"
+                  });
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
